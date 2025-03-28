e77e6c2fdbcc639bf7c6ccd031f56a6738b22efc
Boukind Rachid <boukind.r1@gmail.com>
2025-03-28 00:46:13 +0000

    make a report to any book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import text_to_number
 from stats import letters_num
 from stats import analyze_letters
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -9,9 +10,13 @@ def get_book_text(file_path):
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
     # print(text_to_number(text))
     # print(letters_num(text))
-    analyze_letters(text)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        text = get_book_text(f"{sys.argv[1]}")
+        analyze_letters(text)
 
 main()

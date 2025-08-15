8a2c1be205e9914eb123e78457100ae71f5b22d5
Rachid Boukind <boukind.r1@gmail.com>
2025-08-15 02:34:32 +0100

    it can calculate number of characters in a book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
-
+from stats import how_many_char
 def main(path):
     print(f"{get_num_words(path)} words found in the document")
+    print(how_many_char(path))
 
 main("./books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -9,3 +9,12 @@ def get_num_words(filepath):
     for word in text.split():
         i += 1
     return i
+
+def how_many_char(filepath):
+    num_char = {}
+    text = get_book_text(filepath)
+    text = text.lower()
+    for char in text:
+        if char.isalpha():
+            num_char[char] = num_char.get(char, 0) + 1
+    return num_char
